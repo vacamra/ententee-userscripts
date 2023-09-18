0a1bc4cee82a0aa020dc8acf4849e5c5d7693cc1
Václav Čamra <vacamra@gmail.com>
2023-09-18 13:09:00 +0200

    Updated time reports to match existing ones & not create duplicates

--- Redmine-timeReports.js
+++ Redmine-timeReports.js
@@ -4,7 +4,7 @@
 // @match       https://redmine.ententee.com/projects/*/time_entries/new
 // @grant       none
 // @version     1.0
-// @author      -
+// @author      Václav Čamra
 // @description 8/30/2023, 12:26:12 PM
 // ==/UserScript==
 
@@ -26,6 +26,8 @@ content.insertBefore(reportDiv, form)
 
 let reports = []
 let apiKey = null;
+let loadingExisting = false;
+let saving = false
 
 async function ensureApiKeyFetched() {
   if (apiKey != null) return;
@@ -34,14 +36,36 @@ async function ensureApiKeyFetched() {
 }
 
 async function fetchExistingReports(from, to) {
+  /* Example return object
+  {
+    "id": 12107,
+    "project": {
+      "id": 89,
+      "name": "Test projekt"
+    },
+    "user": {
+      "id": 68,
+      "name": "Vaclav Camra"
+    },
+    "activity": {
+      "id": 9,
+      "name": "Development"
+    },
+    "hours": 8,
+    "comments": "AB#59002",
+    "spent_on": "2023-09-15",
+    "created_on": "2023-09-18T10:36:35Z",
+    "updated_on": "2023-09-18T10:36:35Z"
+  }
+  */
   await ensureApiKeyFetched()
   const userId = document.getElementsByClassName("user active")[0].href.split("/")[4]
-  const response = await fetch(`https://redmine.ententee.com/time_entries.json?from=${from}&to=${to}&project_id=${projectName}&user_id=${userId}`, {
+  const response = await fetch(`https://redmine.ententee.com/time_entries.json?from=${from}&to=${to}&project_id=${projectName}&user_id=${userId}&limit=100`, {
     headers: {
       "X-Redmine-API-Key": apiKey
     }
   })
-  return await response.json()
+  return (await response.json()).time_entries
 }
 
 
@@ -56,9 +80,9 @@ async function importData() {
     if (parts[1].length == 1) {
       parts[1] = `0${parts[1]}`
     }
-    return `${parts[2]}-${parts[1]}-${parts[0]}` // to ISO
-  })
-
+    return `${parts[2]}-${parts[1]}-${parts[0]}` // to ISO (YYYY-MM-DD)
+  })  
+  
   const taskRows = table.slice(2)
 
   const tasks = taskRows.filter(row => row[0] !== "")
@@ -75,10 +99,31 @@ async function importData() {
       .filter(obj => obj.time !== 0)
   )
   reports = tasks
+  loadingExisting = true
   renderReports(days)
   inputTextBox.value = ""
+
+  window.onbeforeunload = function() {
+    return
+  }
+
+  await loadAndMatchExisting(days)
 }
 
+async function loadAndMatchExisting(days) {
+  const existingReports = await fetchExistingReports(days[0], days[days.length - 1])
+
+  reports.forEach(report => {
+    report.exists = existingReports.find(existing => 
+      existing.hours == report.time
+      && existing.comments == report.description
+      && existing.spent_on == report.day
+    ) != null
+  })
+
+  loadingExisting = false
+  renderReports(days)
+}
 
 function renderReports(days) {
   reportDiv.textContent = ''
@@ -95,6 +140,7 @@ function renderReports(days) {
           <span>${days[index]}</span>
           ${dayReports.map(report => `
             <div class="box">
+              <span>Status: ${loadingExisting ? "Loading ⏳" : report.exists ? "Already exists ⚠️ (will skip)" : "Ready to create ✅" }</span><br />
               <span>Task: ${report.task ?? "N/A"}</span><br />
               <span>Time: ${report.time}h</span><br />
               <span>Note: ${report.description}</span><br />
@@ -106,10 +152,12 @@ function renderReports(days) {
     </div>
   `
 
-  divContent += '<input id="push-reports" type="button" value="Create"/>'
+  divContent += `<input id="push-reports" type="button" value="Create"/>`
 
   reportDiv.innerHTML = divContent
-  document.getElementById("push-reports").addEventListener("click", pushReports)
+  const button = document.getElementById("push-reports")
+  button.addEventListener("click", pushReports)
+  button.disabled= loadingExisting || (reports.find(rep => !rep.exists) == null)  
 }
 
 async function getProjectId() {
@@ -136,12 +184,14 @@ function getActivityIdMap() {
 }
 
 async function pushReports(sender, e) {
-  sender.enabled = false
+  sender.disabled = true
   await ensureApiKeyFetched()
   const projectId = await getProjectId(apiKey)
   const activityIds = getActivityIdMap()
 
   for (const report of reports) {
+    if (report.exists) continue
+
     await fetch("https://redmine.ententee.com/time_entries.json", {
       method: 'POST',
       headers: {
@@ -160,5 +210,6 @@ async function pushReports(sender, e) {
       })
     })
   }
-  sender.enabled = true
+  sender.disabled = false
+  reportDiv.textContent = 'Reports created'
 }
